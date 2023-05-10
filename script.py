ba1f5cee1d1ad9c9aad39ee77c2309b57bc01b74
Kwisss <68794249+Kwisss@users.noreply.github.com>
2023-05-11 01:56:15 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import re
 import asyncio
+import nltk
+import pickle
+import os
 import modules.shared as shared
 import gradio as gr
+import asyncio
 import subprocess
 import sys
 
@@ -10,91 +14,83 @@ from modules.text_generation import (encode, get_max_prompt_length)
 from modules.extensions import apply_extensions
 
 
-GoogleOutput=None
-RawGoogleString=None
-GoogleString=None
-ShowGoogleString=False
-OverwriteWord=False
-PrintUserInput=False
-PrintWholePrompt=False
-PrintRawGoogleString=False
-PrintGoogleString=True
+Searchstring = None
+nouns = None
+Searchresponse = None
+promptstring = None
+result = None
+Outputinchat = True
+Printprompt = True
+skipnltkisq = False
+   
+GoogleContext1="Keywords: "
+GoogleContext2=""
 
-ChosenWord="Hey Google"
-GoogleContext1="Interesting keywords to use: "
-GoogleContext2="use these keywords to make an extremely long winded answer!"
+# Download necessary nltk data
+nltk.download('nps_chat')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
 
-print("\nThanks for using the GoogleWordcloud extension! If you encounter any bug, Youre on your own! Good luck!")
 
+print("\nThanks for using the GoogleWordcloud extension! If you encounter any bug, Youre on your own! Good luck!\n")
+    
+  
 params = {
-    'ShowGoogleString': True,
-    'OverwriteWord': True,
-    'PrintUserInput': False,
-    'PrintWholePrompt': False,
-    'PrintRawGoogleString': False,
-    'PrintGoogleString': True
+    'Outputinchat': True,
+    'Printprompt': True,
+    'skipnltkisq': False
 }
+def dialogue_act_features(post):
+    features = {}
+    for word in nltk.word_tokenize(post):
+        features['contains({})'.format(word.lower())] = True
+    return features
+
+def generate_binary_feature(label):
+    if label in ['whQuestion', 'yAnswer','ynQuestion']:
+        return True
+    else:
+        return False
+
+if not os.path.exists('featuresets.pickle'):
+    posts = nltk.corpus.nps_chat.xml_posts()[:10000]
+    featuresets = [(dialogue_act_features(post.text), generate_binary_feature(post.get('class'))) for post in posts]
+
+    # Save the featuresets to a file
+    with open('featuresets.pickle', 'wb') as f:
+        pickle.dump(featuresets, f)
+
+# Load the featuresets from the file
+with open('featuresets.pickle', 'rb') as f:
+    featuresets = pickle.load(f)
+
+# get the classifer from the training set
+classifier = nltk.NaiveBayesClassifier.train(featuresets)
+
 
 def input_modifier(string):
     global UserInput
-    global GoogleOutput
-    global RawGoogleString
-    global ChosenWord
+    global Searchstring 
+    global Searchresponse
+    global result
     # Reset Google output shown in the webui
-    RawGoogleString=None
-
+    Searchresponse=None
+    # Take the user input for later
     UserInput=string
-    # Find out if the chosen word appears in the sentence.
-    # If you want to change the chosen word, change "Hey Google"
-    GoogleOutput = re.search(ChosenWord, UserInput)
-
-    if params['ShowGoogleString']:
-        global ShowGoogleString
-        ShowGoogleString=True
-    else:
-        ShowGoogleString=False
-
-    if params['OverwriteWord']:
-        global OverwriteWord
-        OverwriteWord=True
-    else:
-        OverwriteWord=False
-
-    if params['PrintUserInput']:
-        global PrintUserInput
-        PrintUserInput=True
-        print("User input:\n", UserInput)
-    else:
-        PrintUserInput=False
-    
-    if params['PrintWholePrompt']:
-        global PrintWholePrompt
-        PrintWholePrompt=True
-    else:
-        PrintWholePrompt=False
-    
-    if params['PrintRawGoogleString']:
-        global PrintRawGoogleString
-        PrintRawGoogleString=True
-    else:
-        PrintRawGoogleString=False
-
-    if params['PrintGoogleString']:
-        global PrintGoogleString
-        PrintGoogleString=True
-    else:
-        PrintGoogleString=False
-
-    if(GoogleOutput!=None) and not OverwriteWord:
-        shared.processing_message = "*Is searching...*"
-    elif OverwriteWord:
-        shared.processing_message = "*Is searching...*"
-    else:
-        shared.processing_message = "*Is typing...*"
+    # If not skipped find out if the sentence contains a question
+    if not skipnltkisq:
+        features = dialogue_act_features(UserInput)
+        result = classifier.classify(features)
+        if result:
+            shared.processing_message = "*Is searching...*"
+        elif skipnltkisq:
+            shared.processing_message = "*Is searching...*"
+        else:
+            print("\n Is not a question")
+            shared.processing_message = "*Is typing...*"
     return string
-    
-
-    # Default prompt + GoogleString (if requested)
+                
+    # Default prompt supplied by oobabooga
 def custom_generate_chat_prompt(user_input, state, **kwargs):
     impersonate = kwargs['impersonate'] if 'impersonate' in kwargs else False
     _continue = kwargs['_continue'] if '_continue' in kwargs else False
@@ -148,59 +144,32 @@ def custom_generate_chat_prompt(user_input, state, **kwargs):
         rows.append(user_turn_stripped.rstrip(' '))
     elif not _continue:
 
-        #Adding GoogleString
-        if(GoogleOutput!=None) and not OverwriteWord:
-            async def GoogleWordcloud():
-                global UserInput
-                global RawGoogleString
-                global PrintRawGoogleString
-                response = subprocess.Popen(["python", ".\extensions\GoogleWordcloud\scrape.py", UserInput], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                out, err = response.communicate()
-                response.wait()  # Wait for the process to complete
-                if err:
-                    sys.stderr.write(err.decode())
-                response = out.decode()
-                # Select only the bot response from the response dictionary
-                bot_response = response
-                # Remove [^#^] citations in response
-                RawGoogleString = str(bot_response)
-                if PrintRawGoogleString:
-                    print("\nRawGoogleString output:\n", RawGoogleString)
-                return RawGoogleString
-            asyncio.run(GoogleWordcloud())
-            global RawGoogleString
-            global GoogleString
-            global PrintGoogleString
-           # global GoogleContext1
-           # global GoogleContext2
-            GoogleString=GoogleContext1 + RawGoogleString + "\n" + GoogleContext2
-            if PrintGoogleString:
-                print("\nGoogle output + context:\n", GoogleString)
-            rows.append(GoogleString)
-        elif OverwriteWord:
-            async def GoogleWordcloud():
-                global UserInput
-                global RawGoogleString
-                global PrintRawGoogleString
-                response = subprocess.Popen(["python", ".\extensions\Google\scrape.py", UserInput], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                out, err = response.communicate(input=b'kwissbeats')
-                response.wait()  # Wait for the process to complete
-                if err:
-                    sys.stderr.write(err.decode())
-                response = out.decode()
-                # Select only the bot response from the response dictionary
-                bot_response = response
-                # Remove [^#^] citations in response
-                RawGoogleString = str(bot_response)
-                if PrintRawGoogleString:
-                    print("\nRawGoogleString output:\n", RawGoogleString)
-                return RawGoogleString
-            asyncio.run(GoogleWordcloud())
-            GoogleString = (GoogleContext1 or "") + (RawGoogleString or "") + "\n" + (GoogleContext2 or "")
-            if PrintGoogleString:
-                print("\nGoogle output + context:\n", GoogleString)
-            rows.append(GoogleString)
+        
 
+
+        #Adding GoogleString
+        if result or skipnltkisq:
+            async def GoogleWordcloud():
+                global UserInput
+                global Searchstring
+                global Searchresponse
+                global nouns
+                tokens = nltk.word_tokenize(user_input)
+                tagged = nltk.pos_tag(tokens)
+                nouns = [word for word, pos in tagged if pos.startswith('N')]                
+                nouns_str = ' '.join(nouns)
+                print("\n google search terms:", nouns_str)
+                Searchresponse = subprocess.Popen(["python", ".\extensions\GoogleWordcloud\scrape.py",  nouns_str], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = Searchresponse.communicate()
+                if err:
+                    sys.stderr.write(err.decode())
+                    print(err.decode())          
+                Searchresponse = out.decode()
+            asyncio.run(GoogleWordcloud())
+            promptstring = (GoogleContext1 or "") + (Searchresponse or "") + (GoogleContext2 or "")
+            if Printprompt:
+                print("\nGoogle output + context:\n", promptstring)
+            rows.append (promptstring)
         # Adding the user message
         if len(user_input) > 0:
             rows.append(replace_all(user_turn, {'<|user-message|>': user_input.strip(), '<|round|>': str(len(shared.history["internal"]))}))
@@ -213,28 +182,22 @@ def custom_generate_chat_prompt(user_input, state, **kwargs):
 
     prompt = ''.join(rows)
     if also_return_rows:
-        if PrintWholePrompt:
-            print("Prompt:\n", prompt)
         return prompt, rows
     else:
-        if PrintWholePrompt:
-            print("Prompt:\n", prompt)
         return prompt
     
-
 def output_modifier(string):
     """
     This function is applied to the model outputs.
     """
-    global GoogleOutput
-    global RawGoogleString
-    global ShowGoogleString
-    if ShowGoogleString:
-        string = "Google:" + str(RawGoogleString) + "\n\n\n" + string
+    global Searchresponse
+    global Outputinchat
+    if Outputinchat:
+        string = "Google:" + str(Searchresponse) + "\n\n\n" + string
         return string
     else:
         return string
-
+    return string
 
 def bot_prefix_modifier(string):
     """
@@ -244,12 +207,6 @@ def bot_prefix_modifier(string):
     """
     
     return string
-
-
-def FunChooseWord(CustomWordRaw):
-    global ChosenWord
-    ChosenWord = CustomWordRaw
-    return CustomWordRaw
 
 def Context1Func(Context1Raw):
     global GoogleContext1
@@ -261,22 +218,21 @@ def Context2Func(Context2Raw):
     GoogleContext2 = Context2Raw
     return Context2Raw
 
-
 def ui():
-    with gr.Accordion("Instructions", open=False):
+    with gr.Accordion("Instructions", open=True):
         with gr.Box():
             gr.Markdown(
                 """
-                To use it, just start the prompt with Hey Google; it doesn't start if you don't use uppercase and lowercase as in the example. You can change the activation word from GoogleWordcloud options. If the output is strange turn on Show Google Output to see the result of Google, maybe you need to correct your question.
+                To use it, just ask a question. You can skip question detection to always search. If the output is strange turn on Show Google Output to see the result of Google, maybe you need to correct your question.
                 
                 """)
-            
     with gr.Accordion("GoogleWordcloud options", open=True):
         with gr.Row():
-            ShowGoogleString = gr.Checkbox(value=params['ShowGoogleString'], label='Show Google Output')
+            Outputinchat = gr.Checkbox(value=params['Outputinchat'], label='Show Google output in chat')
         with gr.Row():
-            WordOption = gr.Textbox(label='Choose and use a word to activate Google', placeholder="Choose your word. Empty = Hey Google")
-            OverwriteWord = gr.Checkbox(value=params['OverwriteWord'], label='Overwrite Activation Word. Google will always search, ignoring the activation word.')
+            skipnltkisq = gr.Checkbox(value=params['skipnltkisq'], label='Skip Question detection')
+        with gr.Row():
+            Printprompt = gr.Checkbox(value=params['Printprompt'], label='Print bot instruction in console')
         with gr.Accordion("GoogleWordcloud context", open=False):
             with gr.Row():
                 Context1Option = gr.Textbox(label='Choose Google context-1', placeholder="First context, is injected before the Google output. Empty = default context-1")
@@ -285,28 +241,12 @@ def ui():
             with gr.Row():
                 gr.Markdown(
                     """
-                    You can see the default context (with Google output in the middle) by turning on the fourth option in "Print in console options": "Print Google string in command console".
+                    You can see the default context (with Google output in the middle) if you check: Skip Question detection.
                     """)
-            
-    with gr.Accordion("Print in console options", open=True):
-        with gr.Row():
-            PrintUserInput = gr.Checkbox(value=params['PrintUserInput'], label='Print User input in command console. The user input will be fed first to Google, and then to the default bot.')
-        with gr.Row():
-            PrintWholePrompt = gr.Checkbox(value=params['PrintWholePrompt'], label='Print whole prompt in command console. Prompt has: context, Google search output, and user input.')
-        with gr.Row():
-            PrintRawGoogleString = gr.Checkbox(value=params['PrintRawGoogleString'], label='Print raw Google string in command console. The raw Google string is the clean Google output.')
-        with gr.Row():
-            PrintGoogleString = gr.Checkbox(value=params['PrintGoogleString'], label='Print Google string in command console. It is the Google output + a bit of context, to let the default bot understand what to do with it.')
-    
 
-    ShowGoogleString.change(lambda x: params.update({"ShowGoogleString": x}), ShowGoogleString, None)
-    WordOption.change(fn=FunChooseWord, inputs=WordOption)
-    OverwriteWord.change(lambda x: params.update({"OverwriteWord": x}), OverwriteWord, None)
-    
+    Outputinchat.change(lambda x: params.update({"Outputinchat": x}), Outputinchat, None)
+    skipnltkisq.change(lambda x: params.update({"skipnltkisq": x}), skipnltkisq, None)
+    Printprompt.change(lambda x: params.update({"Printprompt": x}), Printprompt, None)
+
     Context1Option.change(fn=Context1Func, inputs=Context1Option)
     Context2Option.change(fn=Context2Func, inputs=Context2Option)
-
-    PrintUserInput.change(lambda x: params.update({"PrintUserInput": x}), PrintUserInput, None)
-    PrintWholePrompt.change(lambda x: params.update({"PrintWholePrompt": x}), PrintWholePrompt, None)
-    PrintRawGoogleString.change(lambda x: params.update({"PrintRawGoogleString": x}), PrintRawGoogleString, None)
-    PrintGoogleString.change(lambda x: params.update({"PrintGoogleString": x}), PrintGoogleString, None)
